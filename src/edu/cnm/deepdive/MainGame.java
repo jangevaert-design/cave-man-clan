c081ee3901ae6ede5f300fb94117b946dad76290
Jan Gevaert <jan.gevaert@gmail.com>
2021-05-06 11:55:02 -0600

    Guess a number game

---
+++ src/edu/cnm/deepdive/MainGame.java
@@ -0,0 +1,33 @@
+package edu.cnm.deepdive;
+
+
+public class MainGame {
+
+  public static void main(String[] args) {
+    System.out.println("Hello and welcome to the cave man clan!");
+
+/*    Random random = new Random();
+    int theSecretNumber = random.nextInt(100);
+    int yourGuess = 0;
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (theSecretNumber != yourGuess) {
+      System.out.println("Guess a number between 0 and 100");
+      //get input from the user
+      yourGuess = scanner.nextInt();
+
+      //check to see if the guess was correct
+      if (yourGuess < theSecretNumber) {
+        System.out.println("Guess higher!");
+      } else if (yourGuess > theSecretNumber) {
+        System.out.println("Guess lower!");
+      } else {
+        System.out.println("Spot on!!");
+      }
+
+    }
+
+ */
+  }
+}
